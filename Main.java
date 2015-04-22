372baa9a313d6627e3007ce7ce79ecf3c448840c
frank52 <frank52@live.nl>
2015-04-22 15:07:57 +0200

    main

--- Main.java
+++ Main.java
@@ -1,41 +1,32 @@
 package ASRS;
+
 import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
-
 public class Main {
-	
-		public static void main(String[] args) throws Exception{
-			
-			//Hoofdscherm openen
-			Scherm scherm = new Scherm();
-			scherm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			scherm.setVisible(true);
-			
-			// File Explorerer settings
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			
-                        
+
+    public static void main(String[] args) throws Exception {
+
+        //Hoofdscherm openen
+        Scherm scherm = new Scherm();
+        scherm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        scherm.setVisible(true);
+
+        // File Explorerer settings
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
                         //pakbon testen
-                        Klant klant = new Klant("Piet", " van Jansen", "Kalverstraat 17", "1012NX", "Amsterdam");
-                        ArrayList<Integer> artikelnrs = new ArrayList();
-                        artikelnrs.add(3);
-                        artikelnrs.add(5);
-                        artikelnrs.add(8);
-                        artikelnrs.add(1);
-                        artikelnrs.add(2);
-                        System.out.println(artikelnrs);
-                        Pakbon pakbon = new Pakbon(klant, 3, artikelnrs);
-                        pakbon.generatePakbon();
-			
-			
-	        
-	        
-	        
-			
-		}
+//                        Klant klant = new Klant("Piet", " van Jansen", "Kalverstraat 17", "1012NX", "Amsterdam");
+//                        ArrayList<Integer> artikelnrs = new ArrayList();
+//                        artikelnrs.add(3);
+//                        artikelnrs.add(5);
+//                        artikelnrs.add(8);
+//                        artikelnrs.add(1);
+//                        artikelnrs.add(2);
+//                        System.out.println(artikelnrs);
+//                        Pakbon pakbon = new Pakbon(klant, 3, artikelnrs);
+//                        pakbon.generatePakbon();
+    }
 
-		
-	}
-
+}
