64b899bced098dc6d23ca805f3d132ddc60e1b1f
wfikkert <wfikkert@gmail.com>
2015-04-24 13:54:39 +0200

    database

--- Database.java
+++ Database.java
@@ -1,8 +1,39 @@
 package ASRS;
 
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+
 /**
  * Created by Willem on 24-4-2015.
  */
 public class Database {
-    
+    String dbUrl = "jdbc:mysql://stephanschrijver.nl:3306/stephhq105_kbs";
+    String driver = "com.mysql.jdbc.Driver";
+    String user = "stephhq105_kbs";
+    String pass = "WKJ03";
+
+
+
+    public void DatabaseBestelling() {
+
+
+        try {
+            Class.forName(driver).newInstance();
+            Connection database = DriverManager.getConnection(dbUrl, user, pass);
+            Statement myStmt = database.createStatement();
+
+        int rowsAffected = myStmt.executeUpdate(
+                "insert into bestelling" +
+                        "('ordern', 'datum', 'artikel', 'artikel_locatie' )"+
+                        "values"+
+                                "('32', '24-4-2015', '5', 'A5')"
+        );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
+
