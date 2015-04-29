02381d8a20e4776755a1e8b3ab25fb102013e3da
xViMeSx <maarten.kuik@windesheim.nl>
2015-04-29 11:51:33 +0200

    skfdhhkdfsfds

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         //Artikellen in array zetten
-        Artikel artikel =
+        Artikel artikel = getlistArtikel();
 
 
 
