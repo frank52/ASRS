6730799865578d5949869b1d493d1c84c6b083db
wfikkert <wfikkert@gmail.com>
2015-05-01 18:58:43 +0200

    opnieuw beginnen met tekenpanel

--- TekenPanel.java
+++ TekenPanel.java
@@ -13,6 +13,8 @@ public class TekenPanel extends JPanel{
     private int ySelf = 20; //startpunt y
     private Vak[][] alleVakken;
     private Color myColor;
+    private int y=0;
+    private int x=0;
 
     public TekenPanel(){
 
@@ -23,21 +25,23 @@ public class TekenPanel extends JPanel{
 
         super.paintComponent(g);
         setBackground(Color.WHITE);
+
         tekenMagazijn(g);
-        generateVakken(5);
-        kleurVakjes();
-        tekenMagazijn(g);
-        for(Artikel a : d1.getlistLocatie()) {
-            System.out.println(a.getPlaats());
-        }
+//        kleurVakjes();
+
+
+//        for(Artikel a : d1.getlistLocatie()) {
+//            System.out.println(a.getPlaats());
+//        }
+
     }
 
-    public void tekenMagazijn(Graphics g) {
+    public void tekenMagazijn(Graphics g){
         for(int i1 = 0; i1 < 5; i1++) {
             for(int i = 0; i < 5; i++) {
                 g.setColor(Color.black);
                 g.drawRect(xSelf, ySelf, verhouding, verhouding);
-                g.setColor(myColor);
+                g.setColor(Color.white);
                 g.fillRect(xSelf+1, ySelf+1, verhouding-1, verhouding-1);
                 xSelf = xSelf + verhouding;
 
@@ -49,34 +53,50 @@ public class TekenPanel extends JPanel{
         ySelf = 20;
     }
 
-    private void generateVakken(int s)
-    {
-        int col=0;
-        int row;
 
-        alleVakken = new Vak[s][s];
-        for (row = 0; row < alleVakken.length; row++) {
-            for (col = 0; col < alleVakken[row].length; col++)
-            {
-                Vak myVak = new Vak(col, row);
-                alleVakken[row][col] = myVak;
+//    private void generateVakken(Graphics g, int s)
+//    {
+//        for(int i1 = 0; i1 < 5; i1++) {
+//            for(int i = 0; i < 5; i++) {
+//                g.setColor(Color.black);
+//                g.drawRect(xSelf, ySelf, verhouding, verhouding);
+//                g.setColor(myColor);
+//                g.fillRect(xSelf+1, ySelf+1, verhouding-1, verhouding-1);
+//                xSelf = xSelf + verhouding;
+//
+//            }
+//            xSelf =20;
+//            ySelf = ySelf + verhouding;
+//        }
+//        xSelf = 20;
+//        ySelf = 20;
+//
+//        int col=0;
+//        int row;
+//
+//        alleVakken = new Vak[s][s];
+//        for (row = 0; row < alleVakken.length; row++) {
+//            for (col = 0; col < alleVakken[row].length; col++)
+//            {
+//                Vak myVak = new Vak(col, row);
+//                alleVakken[row][col] = myVak;
 //                System.out.println(alleVakken[row][col].toString());
-            }
-        }
-    }
-    Locatie l1 = new Locatie(5,5);
-    Database d1 =  new Database();
+//            }
+//        }
+//    }
 
-    public void kleurVakjes(){
-        d1.SelectLocatie();
-        for(Artikel a: d1.getlistLocatie()) {
-            if (l1.toString().equals(a.getPlaats())) {
-                myColor = Color.RED;
-            } else {
-                myColor = Color.white;
-            }
-        }
-    }
+//    Database d1 =  new Database();
+//
+//    public void kleurVakjes(){
+//        d1.SelectLocatie();
+//        for(Artikel a: d1.getlistLocatie()) {
+//            if (alleVakken.equals(a.getPlaats())) {
+//                myColor = Color.RED;
+//            } else {
+//                myColor = Color.white;
+//            }
+//        }
+//    }
 
 
 }
