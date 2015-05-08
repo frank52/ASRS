eaec3984d37d37a5722f3689845fde6e90d1b5a4
sschrijver <stephanschrijver@gmail.com>
2015-05-08 13:50:58 +0200

    Test
    Zou moeten werken met goede data in de artikel4 arraylist

--- TekenPanel.java
+++ TekenPanel.java
@@ -7,18 +7,19 @@ import java.util.ArrayList;
 /**
  * Created by Willem on 1-5-2015.
  */
-public class TekenPanel extends JPanel{
+public class TekenPanel extends JPanel
+{
 
     private int verhouding = 100; //100 bij 100
     private int xSelf = 20; //startpunt x
     private int ySelf = 20; //startpunt y
-    private Vak[][] alleVakken;
     private Color myColor;
-    private int i1;
-    private int i;
-    private ArrayList<String> locatie1 = new ArrayList<>();
 
-    public TekenPanel(){
+    private ArrayList<String> locatie1 = new ArrayList<>();
+    Database d1 = new Database();
+
+    public TekenPanel()
+    {
 
     }
 
@@ -29,52 +30,65 @@ public class TekenPanel extends JPanel{
         setBackground(Color.WHITE);
 
         tekenMagazijn(g);
-//        for(Artikel a : d1.getlistLocatie()) {
-//            System.out.println(a.getPlaats());
-//        }
 
     }
 
-    public void tekenMagazijn(Graphics g){
-        for( i1= 0; i1 < 5; i1++) {
-            for(i = 0; i < 5; i++) {
+    public void tekenMagazijn(Graphics g)
+    {
+
+        ArrayList<Artikel> artikelen = d1.getlistLocatie();
+
+        for (int i1 = 0; i1 < 5; i1++)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                String locatie = "(" + i1 + "," + i + ")";
+                locatie1.add(locatie);
+
                 g.setColor(Color.black);
                 g.drawRect(xSelf, ySelf, verhouding, verhouding);
-                g.setColor(myColor);
+
+                if (tekenVakjes(locatie))
+                {
+                    g.setColor(Color.RED);
+                }
+                else
+                {
+                    g.setColor(Color.WHITE);
+                }
+
                 g.fillRect(xSelf + 1, ySelf + 1, verhouding - 1, verhouding - 1);
                 xSelf = xSelf + verhouding;
-                String locatie= "("+i1+","+i+")";
-                locatie1.add(locatie);
-                kleurVakjes();
 
             }
-            xSelf =20;
+            xSelf = 20;
             ySelf = ySelf + verhouding;
-            kleurVakjes();
         }
         xSelf = 20;
         ySelf = 20;
-        kleurVakjes();
     }
 
-    Database d1 =  new Database();
+    public boolean tekenVakjes(String s)
+    {
+        boolean returnBoolean;
+        returnBoolean = false;
 
-    public void kleurVakjes(){
         d1.SelectLocatie();
-        for(Artikel a: d1.getlistLocatie()) {
-            for (String a1 : locatie1) {
-                if (locatie1.equals(a.getPlaats())) {
-                    myColor = Color.RED;
-                } else {
-                    myColor = Color.white;
-                }
-            }
-        }
-    }
 
+        ArrayList<Artikel> artikelLijst = d1.getlistLocatie();
+
+        for (Artikel a : artikelLijst)
+        {
+            System.out.println(a.getPlaats());
+            System.out.println("Check: '" + a.getPlaats() + "' equals '" + s + "'");
+            if (a.getPlaats() == s)
+            {
+                returnBoolean = true;
+                System.out.println("TRUE");
+            }
+
+        }
+        return returnBoolean;
+    }
 
 }
-
-
-
-
